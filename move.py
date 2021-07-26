59e055567391692c842c3ba25f4b476c25d8a650
chris alvizuris <chris.alvizuris@gmail.com>
2021-07-25 19:48:31 -0700

    Added comments to move file

--- move.py
+++ move.py
@@ -6,8 +6,10 @@ from time import sleep
 drone = tello.Tello()
 drone.connect()
 
+# get_battery will return the battery percentage in an integer range between 0-100
 print(drone.get_battery())
 
+# takeoff, move for 50 centimeters in 2 seconds, and then land
 drone.takeoff()
 drone.send_rc_control(0, 50, 0, 0)
 sleep(2)
