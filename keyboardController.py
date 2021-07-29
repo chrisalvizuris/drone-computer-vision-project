8c1b2fe35d0e3b9e0b0ca8e613455aae8887b871
chris alvizuris <chris.alvizuris@gmail.com>
2021-07-29 11:27:10 -0700

    Enabled keyboard controls for manual control of yaw velocity and vertical movements.

--- keyboardController.py
+++ keyboardController.py
@@ -7,19 +7,41 @@ drone = tello.Tello()
 drone.connect()
 print(drone.get_battery())
 
-drone.takeoff()
-
 
 def get_key_input():
-    lr, fb, ud, yv = 0, 0, 0, 0
+    left_right, forward_backward, up_down, yaw_velocity = 0, 0, 0, 0
     speed = 50
 
-    if km.get_key('LEFT'): lr = -speed
-    elif km.get_key('RIGHT'): lr = speed
+    if km.get_key('LEFT'):
+        left_right = -speed
+    elif km.get_key('RIGHT'):
+        left_right = speed
 
-    if km.get_key('UP'): ud = -speed
-    elif km.get_key('DOWN'): ud = speed
+    if km.get_key('UP'):
+        forward_backward = speed
+    elif km.get_key('DOWN'):
+        forward_backward = -speed
+
+    if km.get_key('w'):
+        up_down = speed
+    elif km.get_key('s'):
+        up_down = -speed
+
+    if km.get_key('a'):
+        yaw_velocity = speed
+    elif km.get_key('d'):
+        yaw_velocity = -speed
+
+    if km.get_key('q'):
+        drone.land()
+
+    if km.get_key('t'):
+        drone.takeoff()
+
+    return [left_right, forward_backward, up_down, yaw_velocity]
 
 
 while True:
-    drone.send_rc_control(0, 0, 0, 0)
+    vals = get_key_input()
+    drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    sleep(0.5)
